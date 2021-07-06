f21646305e719c81a2ac8f88a10349471b3260cc
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-07-05 19:13:00 -0700

    configure email route

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/email", require("./routes/api/email"));
+
+const port = process.env.PORT || 8080;
+app.listen(8080, () => {
+  console.log(`The server is listening on port ${port}`);
+});
---
+++ routes/api/email.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const router = express.Router();
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const SENDER_USER = process.env.SENDER_USER;
+const SENDER_PASSWORD = process.env.SENDER_PASSWORD;
+const EMAIL_TO = process.env.EMAIL_TO;
+
+// @route POST api/posts
+// @desc Receives an email payload
+// @access Public
+router.post("/", async (req, res) => {
+  const { email, name, body } = req.body;
+
+  if (!email || !name)
+    return res.status(400).json({ message: "Email and name are missing" });
+
+  try {
+    const testAccount = await nodemailer.createTestAccount();
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp-mail.outlook.com", // hostname
+      secureConnection: false, // TLS requires secureConnection
+      port: 587,
+      tls: {
+        ciphers: "SSLv3",
+      },
+      auth: {
+        user: SENDER_USER,
+        pass: SENDER_PASSWORD,
+      },
+    });
+
+    const info = await transporter.sendMail({
+      from: `"Email Submission" <${SENDER_USER}>`,
+      to: `${EMAIL_TO}`,
+      subject: "Contact Form Submission",
+      text: `Name: ${name}, Email: ${email}, Subject: ${body}`,
+    });
+
+    return res.status(200).json({ message: "Email successfully sent!" });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ message: "Email transporter failed.", error: error });
+  }
+});
+
+module.exports = router;
